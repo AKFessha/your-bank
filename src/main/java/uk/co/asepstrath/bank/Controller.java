b2a53def2fbfa5411a7e35aacacdd95ef230daaa
Shaun <shaun.greer.2018@uni.strath.ac.uk>
2020-02-10 17:27:58 +0000

    Lab 3 complete

--- src/main/java/uk/co/asepstrath/bank/Controller.java
+++ src/main/java/uk/co/asepstrath/bank/Controller.java
@@ -2,9 +2,7 @@ package uk.co.asepstrath.bank;
 import com.google.gson.Gson;
 import io.jooby.ModelAndView;
 import io.jooby.annotations.*;
-import uk.co.asepstrath.bank.example.TestPerson;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -31,33 +29,9 @@ public class Controller {
     public ModelAndView accounts() {
         Map<String, Object> model = new HashMap<>();
 
-        Account Monica = new Account(100, "Monica");
-
-        Account[] accountsArr = new Account[1];
-        accountsArr[0] = Monica;
-
-        model.put("accountsArr", accountsArr);
+        model.put("accounts", l);
 
         return new ModelAndView("accounts.hbs", model);
 
     }
-
-    @GET("/test")
-    public ModelAndView test(){
-        Map<String, Object> model = new HashMap<>();
-
-        TestPerson t1 = new TestPerson("John", "Smith");
-        TestPerson t2 = new TestPerson("Bob", "Jones");
-
-        TestPerson[] myArray = new TestPerson[2];
-        myArray[0] = t1;
-        myArray[1] = t2;
-        model.put("myArray", myArray);
-
-        return new ModelAndView("test.hbs", model);
-    }
-
-
-
-
 }
