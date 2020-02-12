343ccafa39458b3222cf589e252f61f0f6bdce1c
Radina Kraeva <radinar.kraeva@gmail.com>
2020-02-12 16:37:13 +0000

    Updated database of accounts

--- src/main/java/uk/co/asepstrath/bank/Controller.java
+++ src/main/java/uk/co/asepstrath/bank/Controller.java
@@ -13,12 +13,17 @@ public class Controller {
     private List<Account> l = new ArrayList<>();
     public Controller(){
 
-        l.add(new Account(50, "Rachel"));
-        l.add(new Account(100, "Monica"));
-        l.add(new Account(76, "Phoebe"));
-        l.add(new Account(BigDecimal.valueOf(23.90), "Joey"));
-        l.add(new Account(BigDecimal.valueOf(54.32), "Ross"));
-        l.add(new Account(3, "Chandler"));
+        l.add(new Account(BigDecimal.valueOf(50.00), "Rachel Green"));
+        l.add(new Account(BigDecimal.valueOf(100.00), "Monica Gellar"));
+        l.add(new Account(BigDecimal.valueOf(76.00), "Phoebe Buffay"));
+        l.add(new Account(BigDecimal.valueOf(23.90), "Joey Tribbiani"));
+        l.add(new Account(BigDecimal.valueOf(54.32), "Ross Gellar"));
+        l.add(new Account(BigDecimal.valueOf(3.00), "Chandler Bing"));
+        l.add(new Account(BigDecimal.valueOf(51500.00), "Barney Stinson"));
+        l.add(new Account(BigDecimal.valueOf(330.45), "Ted Mosby"));
+        l.add(new Account(BigDecimal.valueOf(756.80), "Lily Aldrin"));
+        l.add(new Account(BigDecimal.valueOf(254.00), "Marshall Eriksen"));
+        l.add(new Account(BigDecimal.valueOf(1635.52), "Robin Scherbatsky"));
     }
 
     public List<Account> getList(){
