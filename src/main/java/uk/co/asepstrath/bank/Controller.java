a88220cfa72ff8136491a82a6b276dcfdc90c399
Jack Donnelly <JDonnelly@WalkerPrecision.com>
2020-02-12 14:56:39 +0000

    Movedf list to controller and made controller show the account data

--- src/main/java/uk/co/asepstrath/bank/Controller.java
+++ src/main/java/uk/co/asepstrath/bank/Controller.java
@@ -1,10 +1,10 @@
 package uk.co.asepstrath.bank;
 import com.google.gson.Gson;
-import io.jooby.ModelAndView;
 import io.jooby.annotations.*;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/account")
 public class Controller {
@@ -17,30 +17,17 @@ public class Controller {
         l.add(new Account(BigDecimal.valueOf(23.90), "Joey"));
         l.add(new Account(BigDecimal.valueOf(54.32), "Ross"));
         l.add(new Account(3, "Chandler"));
+
     }
 
-    public List<Account> getList(){
-        return l;
-    }
-
-    @GET("/list")
-    public String list(){
-        return l.toString();
-    }
-
-    @GET("/json")
-    public String jasonEnd(){
+    @GET
+    public String Accounts(){
         String json = new Gson().toJson(l );
         return json;
     }
 
-    @GET("/moustache")
-    public ModelAndView accounts() {
-        Map<String, Object> model = new HashMap<>();
 
-        model.put("accounts", l);
 
-        return new ModelAndView("accounts.hbs", model);
 
-    }
+
 }
