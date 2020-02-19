19f22250b68a839088eeb93d14ad6100effe4aa1
Jack Donnelly <JDonnelly@WalkerPrecision.com>
2020-02-19 16:30:02 +0000

    Added test for HTTPResponse Code on VFA

--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -17,6 +18,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @JoobyTest(App.class)
 public class IntegrationTest {
     static OkHttpClient client = new OkHttpClient();
+    @Test
+    public void testVFA() {
+        Request req = new Request.Builder()
+                .url("http://localhost:" + "8080"+"/account/VFA")
+                .build();
+            try (Response rsp = client.newCall(req).execute()) {
+                assertEquals(200,rsp.code());
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
     @Test
     public void testJson() throws JSONException {
         Request req = new Request.Builder()
