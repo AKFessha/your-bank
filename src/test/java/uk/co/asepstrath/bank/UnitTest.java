009fc5614147658fa6ef42e369a39dfedc8911cd
Sarah Ryan CSM2018 <sarah.corrigan.2018@uni.strath.ac.uk>
2020-02-26 16:28:29 +0000

    Updated homepage

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,25 +1,5 @@
 package uk.co.asepstrath.bank;
 
-import io.jooby.StatusCode;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.apache.commons.io.IOUtils;
-import org.json.JSONException;
-import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 public class UnitTest {
-
-
-
-
-
 
 }
